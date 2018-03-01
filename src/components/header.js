4ac90fe473386a92294fb6b55e518e74ace1f0e9
Your Name <you@example.com>
2018-03-01 20:11:06 +0100

    style changes

--- src/components/header.js
+++ src/components/header.js
@@ -11,13 +11,17 @@ class Header extends React.Component {
           <div className={css.logoBox}>
             <h1>Fit Net</h1>
           </div>
-
-          {/* COMING SOON <div className={css.loginBox}>
+          <div className={css.loginBox}>
             <Login />
-          </div> */}
+          </div>
         </div>
         <div className={css.headerBox2}>
           <Navigation />
+        </div>
+        <div className={css.loginModalWrapper}>
+          <div className={css.loginModalHolder}>
+
+          </div>
         </div>
       </div>
     )
