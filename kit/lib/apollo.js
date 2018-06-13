e0a8a204d910a2c94766041a0fec0842b568a0eb
lookagit <simjanovic.luka@gmail.com>
2018-06-13 22:15:38 +0200

    added https

--- kit/lib/apollo.js
+++ kit/lib/apollo.js
@@ -42,7 +42,7 @@ export function getNetworkInterface(uri, opt) {
 export function browserClient() {
   // If we have an internal GraphQL server, we need to append it with a
   // call to `getServerURL()` to add the correct host (in dev + production)
-  const uri = 'http://apps.fit-net.rs/graphql';
+  const uri = 'https://apps.fit-net.rs/graphql';
 
   return createClient({
     networkInterface: getNetworkInterface(uri),
